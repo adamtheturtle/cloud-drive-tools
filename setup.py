c1b64d758175a76d1f95fe07de935f81e6aeef5d
Adam Dangoor <adamdangoor@gmail.com>
2018-08-17 23:05:39 +0100

    Move files around

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@
 Setup script for Cloud Drive Tools.
 """
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='Cloud Drive Tools',
     author='Adam Dangoor',
     author_email='adamdangoor@gmail.com',
     install_requires=['click', 'PyYAML'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     entry_points="""
         [console_scripts]
-        cloud-drive-tools=cloud_drive_tools:cloud_drive_tools
+        cloud-drive-tools=cloud_drive_tools
     """,
 )
