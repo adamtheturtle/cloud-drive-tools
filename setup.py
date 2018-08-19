04759877ca780e603ab7c712dc17bfe489781354
Adam Dangoor <adamdangoor@gmail.com>
2018-08-19 11:55:10 +0100

    Get requirements from requirements files

--- setup.py
+++ setup.py
@@ -4,11 +4,23 @@ Setup script for Cloud Drive Tools.
 
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as requirements:
+    INSTALL_REQUIRES = []
+    for line in requirements.readlines():
+        if not line.startswith('#'):
+            INSTALL_REQUIRES.append(line)
+
+with open('dev-requirements.txt') as dev_requirements:
+    DEV_REQUIRES = []
+    for line in dev_requirements.readlines():
+        if not line.startswith('#'):
+            DEV_REQUIRES.append(line)
+
 setup(
     name='cloud-drive-tools',
     author='Adam Dangoor',
     author_email='adamdangoor@gmail.com',
-    install_requires=['click', 'PyYAML'],
+    install_requires=INSTALL_REQUIRES,
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     entry_points="""
