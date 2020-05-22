b7acdd3090eb4bdb54c203cfc214a71cea48cb5c
Adam Dangoor <adamdangoor@gmail.com>
2020-05-22 12:12:10 +0100

    Fix for relative mount

--- src/cloud_drive_tools/cloud_drive_tools.py
+++ src/cloud_drive_tools/cloud_drive_tools.py
@@ -552,7 +552,7 @@ def _wait_for_remote_mount(
     Wait for the rclone mount or error if it does not start within some time
     close to 25 seconds.
     """
-    remote_mount = remote_encrypted / path_on_cloud_drive
+    remote_mount = remote_encrypted / Path(path_on_cloud_drive).relative_to('/')
     attempts = 0
     max_attempts = 5
     sleep_seconds = 5
