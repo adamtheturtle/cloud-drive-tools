587b3dde16d1cce70cb22cfab4bbae1addca0947
Adam Dangoor <adamdangoor@gmail.com>
2020-08-25 08:11:24 +0100

    Use a lint setup more similar to my other tools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Setup script for Cloud Drive Tools.
 """
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open('requirements.txt') as requirements:
     INSTALL_REQUIRES = []
@@ -17,17 +17,8 @@ with open('dev-requirements.txt') as dev_requirements:
             DEV_REQUIRES.append(line)
 
 setup(
-    name='cloud-drive-tools',
-    author='Adam Dangoor',
-    author_email='adamdangoor@gmail.com',
     install_requires=INSTALL_REQUIRES,
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
     extras_require={
         'dev': DEV_REQUIRES,
     },
-    entry_points="""
-        [console_scripts]
-        cloud-drive-tools=cloud_drive_tools:cloud_drive_tools
-    """,
 )
